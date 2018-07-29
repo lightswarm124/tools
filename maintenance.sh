b67367239b56a7be6f03f594595f816b138faf40
lightswarm124 <lightswarm@gmail.com>
2018-07-29 19:11:01 -0400

    new bash scripts

---
+++ maintenance.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt-get check
+sudo apt-get -f install
+sudo apt-get autoremove
+sudo apt-get -y autoclean
+sudo apt-get -y clean
---
+++ varlock.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#sudo rm /var/lib/apt/list/lock
+sudo rm /var/cache/apt/archives/lock
+sudo rm /var/lib/dpkg/lock
+sudo rm -rf /var/lib/apt/lists/
