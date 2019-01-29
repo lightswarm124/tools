54fcd4d819629ccaf1821eb0f8f978c09c563ea5
lightswarm124 <lightswarm@gmail.com>
2019-01-29 12:22:25 -0500

    updated for ubuntu18

--- maintenance.sh
+++ maintenance.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 sudo apt-get check
-sudo apt-get -f install
+sudo apt-get -f -y install
 sudo apt-get autoremove
 sudo apt-get -y autoclean
 sudo apt-get -y clean
